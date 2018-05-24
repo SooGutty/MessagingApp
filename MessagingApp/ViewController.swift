7d7c444fc7d23dcebf58c9d0c52590732cc1c7f7
user138463 <user138463@74-80-245-25.ic2net.net>
2018-05-24 16:09:04 -0700

    Initial Commit

---
+++ MessagingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MessagingApp
+//
+//  Created by user138463 on 5/24/18.
+//  Copyright © 2018 Digital Grounds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
